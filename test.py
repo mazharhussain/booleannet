23834364139e92bf4e482f8b3042bf60e12b8d42
istvan.albert <istvan.albert@5b4be453-893b-0410-8fd3-ef14834d19df>
2007-10-03 16:54:49 +0000

    test reorganization

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+import sys
+sys.path.append( '..' )
+
+class DatafitterLoadTest( unittest.TestCase ):
+    pass
+
+def _test():
+    unittest.main()
+
+if __name__ == '__main__':
+    _test()    
