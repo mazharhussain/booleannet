2c2cffe26fe1e509c6b4a4c1d3f33038e19c6d7c
istvan.albert <istvan.albert@5b4be453-893b-0410-8fd3-ef14834d19df>
2008-09-23 13:04:07 +0000

    added more docs and modified tutorials

--- examples/tutorials/t-03.py
+++ examples/tutorials/t-03.py
@@ -1,6 +1,8 @@
 import boolean
 
-# All initial conditions are sampled. 
+#
+# Random sampling of initial conditions
+#
 # If A is set to False, a steady state is obtained.
 #
 # 
@@ -18,23 +20,29 @@ from boolean import Model
 
 seen = {}
 
-for i in range(100):
+ 
+#
+# the key will be the fingerprint of the first state 
+#(some random inital conditions may be the same), it is fine to overwrite in this case
+# as the 'sync' update rule is completely deterministic
+#
+for i in range(10):
     model = boolean.Model( text, mode='sync')
     model.initialize()
     model.iterate( steps=20 )
 
-    #for state in model.states:
-    #    print state.A, state.B, state.C, state.D
-
+    # detect the cycles in the states
     size, index = model.detect_cycles() 
     
-    
-    seen [ model.first.fp() ] = (index, size, [x.fp() for x in model.states[:4]] )
-    
-    #model.report_cycles()    
+    # fingerprint of the first state
+    key = model.first.fp()
 
-values = seen.values()
-values.sort()
+    # keep only the first 10 states out of the 20
+    values = [ x.fp() for x in model.states[:10] ]
 
-for value in values:
-    print value
+    # store the fingerprinted values for each initial state
+    seen [ key ] = (index, size, values )
+
+# print out the observed states
+for first, values in seen.items():
+    print 'Start: %s -> %s' % (first, values)
