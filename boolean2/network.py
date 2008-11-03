0d21540fb195bcc922da8707ecabf11bc5baeca3
istvan.albert <istvan.albert@5b4be453-893b-0410-8fd3-ef14834d19df>
2008-11-03 20:00:31 +0000

    gml writer adde

--- boolean2/network.py
+++ boolean2/network.py
@@ -2,6 +2,7 @@ import util
 
 try:
     import networkx
+    from networkx.readwrite import gml
 except ImportError:
     util.error( "networkx is missing, install it from https://networkx.lanl.gov/")
 
@@ -24,7 +25,7 @@ class TransGraph(object):
     
         # generating the fingerprints and sto
         fprints = []
-        for state in self.states:
+        for state in states:
             fp = state.fp()
             fprints.append( fp )
             self.store[fp] = state
@@ -35,16 +36,40 @@ class TransGraph(object):
                 self.graph.add_edge(head, tail)
                 self.seen.add(pair)
                 
-
-def add_transitions( model, graph ):
-    pass
-
+    def save_gml(self, fname):
+        "Saves the graph as gml"
+        gml.write_gml(self.graph, fname)
+    
+    def save_info(self, fname):
+        "Saves the info"
+        first = self.store.keys[0]
+        nodes = first.keys()
+        
 def test():
     """
     Main testrunnner
     """
-    tgraph = TransGraph() 
+    import ruleparser
     
+    text = """
+    A = True
+    B = False
+    C = False
+    1: A* = A
+    2: B* = not B
+    3: C* = A and B
+    """
+    model = ruleparser.Model( mode='sync', text=text )
+    model.initialize( missing=util.true )
+    model.iterate( steps = 5 )
+    
+    #for state in model.states:
+    #    print state
+
+    trans = TransGraph() 
+    trans.add( model.states )
+    trans.save_gml( 'test.gml' )
+    print trans.store
 
 if __name__ == '__main__':
     test()
