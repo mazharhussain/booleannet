3195440bd2a0fae96a29859697ca19c299956679
istvan.albert <istvan.albert@5b4be453-893b-0410-8fd3-ef14834d19df>
2008-11-11 17:13:24 +0000

    typo fix

--- branches/boolean2-branch/examples/samples/singlepick.py
+++ branches/boolean2-branch/examples/samples/singlepick.py
@@ -19,7 +19,7 @@ def simulation( trans ):
     "One simulation step will update the transition graph"
 
     # create the model
-    model = boolean2.Model( text=rules, mode='time')
+    model = boolean2.Model( text=rules, mode='async')
 
     # generates all states, set limit to a value to keep only the first that many states
     # when limit is a number it will take the first that many initial states
@@ -35,7 +35,7 @@ def main():
     "This is the main code that runs the simulation many times"
 
     # this will hold the transition graph
-    trans = network.TransGraph( logfile='singlepick.log' )
+    trans = network.TransGraph( logfile='singlepick.log', verbose=True )
 
     # will run the simulation this many times
     for num in range( 30 ):
