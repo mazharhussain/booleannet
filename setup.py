490bd4d4f6729d966bf39adf213b8044722319e3
istvan.albert <istvan.albert@gmail.com>
2014-04-25 14:35:45 +0000

    added scripts

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@
 #
 
 import boolean2
-from distutils.core import setup
+from setuptools import setup
 
-setup( name='BooleanNet',
-       version = '1.2.5',
-       description = 'Boolean Network Simulation Toolbox',
-       author = 'Istvan Albert',
-       author_email = 'istvan.albert@gmail.com',
-       url = 'http://code.google.com/p/booleannet/',
-       packages = [ 'boolean2', 'boolean2.ply' , 'boolean2.plde'],
-     )
+setup(name='BooleanNet',
+      version='1.2.6',
+      description='Boolean Network Simulation Toolbox',
+      author='Istvan Albert',
+      author_email='istvan.albert@gmail.com',
+      url='http://code.google.com/p/booleannet/',
+      packages=['boolean2', 'boolean2.ply', 'boolean2.plde'],
+)
